a25c7761a5c3943af8945627dcaf61501244030d
mennaahmeddd <manooahmed418@gmail.com>
2023-05-23 08:18:06 -0700

    updated

--- string.c
+++ string.c
@@ -88,3 +88,24 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * *_strchr -> Locate character in string
+ *
+ * @s: String
+ * @c: Character
+ *
+ * Return: Depend Condition
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
